f6b9651e8993291da5f214aa664decd187ff31d9
Anshul Vashishth <theavashisht0227@gmail.com>
2024-09-30 10:23:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,136 @@
+const doc = document;
+const menuOpen = doc.querySelector(".menu");
+const menuClose = doc.querySelector(".close");
+const overlay = doc.querySelector(".overlay");
+
+menuOpen.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+});
+
+menuClose.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++
+
+var sliderCounter = 0;
+var sliderContent = [
+  "New to Finance",
+  "Mutual Funds",
+  "Stocks & IPOs",
+  "Fixed Deposits",
+  "Wealth Manager"
+];
+var slider = document.querySelector(".hero-text");
+var sliderValue = document.querySelector("#sliderValue");
+
+function slide() {
+  if (sliderCounter >= sliderContent.length) {
+    sliderCounter = 0;
+  }
+
+  sliderValue.innerHTML = "";
+  
+  sliderValue.classList.remove("holder-animation");
+  void sliderValue.offsetWidth;
+  sliderValue.classList.add("holder-animation");
+  
+  for (i = 0; i < sliderContent[sliderCounter].length; i++) {
+    let letterDiv = document.createElement("div");
+    letterDiv.innerHTML = sliderContent[sliderCounter][i];
+
+    if (letterDiv.innerHTML == " ") {
+      letterDiv.innerHTML = "&nbsp;";
+    }
+    letterDiv.classList.add("start");
+    letterDiv.classList.add("animation");
+    letterDiv.style.animationDelay = i / 10 + "s";
+    sliderValue.appendChild(letterDiv);
+  }
+
+  sliderCounter++;
+}
+
+slide();
+setInterval(slide, 4000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+const MIN_SPEED = 1.5
+const MAX_SPEED = 2.5
+
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min
+}
+
+class Blob {
+  constructor(el) {
+    this.el = el
+    const boundingRect = this.el.getBoundingClientRect()
+    this.size = boundingRect.width
+    this.initialX = randomNumber(0, window.innerWidth - this.size)
+    this.initialY = randomNumber(0, window.innerHeight - this.size)
+    this.el.style.top = `${this.initialY}px`
+    this.el.style.left = `${this.initialX}px`
+    this.vx =
+      randomNumber(MIN_SPEED, MAX_SPEED) * (Math.random() > 0.5 ? 1 : -1)
+    this.vy =
+      randomNumber(MIN_SPEED, MAX_SPEED) * (Math.random() > 0.5 ? 1 : -1)
+    this.x = this.initialX
+    this.y = this.initialY
+  }
+
+  update() {
+    this.x += this.vx
+    this.y += this.vy
+    if (this.x >= window.innerWidth - this.size) {
+      this.x = window.innerWidth - this.size
+      this.vx *= -1
+    }
+    if (this.y >= window.innerHeight - this.size) {
+      this.y = window.innerHeight - this.size
+      this.vy *= -1
+    }
+    if (this.x <= 0) {
+      this.x = 0
+      this.vx *= -1
+    }
+    if (this.y <= 0) {
+      this.y = 0
+      this.vy *= -1
+    }
+  }
+
+  move() {
+    this.el.style.transform = `translate(${this.x - this.initialX}px, ${
+      this.y - this.initialY
+    }px)`
+  }
+}
+
+function initBlobs() {
+  const blobEls = document.querySelectorAll('.bouncing-blob')
+  const blobs = Array.from(blobEls).map((blobEl) => new Blob(blobEl))
+
+  function update() {
+    requestAnimationFrame(update)
+    blobs.forEach((blob) => {
+      blob.update()
+      blob.move()
+    })
+  }
+
+  requestAnimationFrame(update)
+}
+
+initBlobs()
